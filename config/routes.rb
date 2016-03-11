8d8dfeb8abc1ad28d05f5b0eeb55e0a71272b80b
arifeuer <feuer.ari@gmail.com>
2016-03-11 19:27:15 +0000

    rooted app at /forms/new, merged with kishan's push

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   resources :forms
   mount JasmineRails::Engine => '/specs' if defined?(JasmineRails)
+  root 'forms#new'
   
   
   # The priority is based upon order of creation: first created -> highest priority.
