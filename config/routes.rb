cc54a955f1da02382c8c8eaa8503176abb34fec1
jayiyer <jay.iyer@berkeley.edu>
2016-04-09 23:16:50 +0000

    separate page for generating forms

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 Rails.application.routes.draw do
   resources :forms
   mount JasmineRails::Engine => '/specs' if defined?(JasmineRails)
-  root 'forms#new'
+  root 'forms#newform'
   
   
+  
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
