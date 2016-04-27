bb7d389c83e84f1bb5a4fb16e9ab9e37c3405238
kbpatelucb <kbpatel94@berkeley.edu>
2016-04-27 18:46:37 +0000

    dbconnect branch

--- app/controllers/trainings_controller.rb
+++ app/controllers/trainings_controller.rb
@@ -1,4 +1,3 @@
-require 'chronic'
 class TrainingsController < ApplicationController
   #before_action :set_Train, only: [:show, :edit, :update, :destroy]
   skip_before_filter :set_current_user
@@ -21,23 +20,3 @@ end
 
 
 
-#   private
-#     # Use callbacks to share common setup or constraints between actions.
-#     #def set_Train
-#     #  @Train = Train.find(params[:id])
-#     #end
-
-#     # Never trust parameters from the scary internet, only allow the white list through.
-#     def Train_params
-#       params.require(:Train).permit(:start_time, :end_time, :Train_id)
-#     end
-    
-#     #Keys: Train, user, start_time, end_time, description
-#     def json_trainings(instances)
-#       lst = []
-#       instances.each do |Train|
-#         lst << Train.full_json
-#       end
-#       lst
-#     end 
-# end
