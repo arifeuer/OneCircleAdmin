4ecafe3590f2fec18f037527abf6b8166eb08b5f
Corrina Calanoc <corrina.calanoc@berkeley.edu>
2016-04-28 09:56:06 -0700

    adding 100 rows of seed data from joined tables

--- db/schema.rb
+++ db/schema.rb
@@ -11,13 +11,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160428120403) do
+ActiveRecord::Schema.define(version: 20160415091302) do
 
   create_table "forms", force: :cascade do |t|
     t.string   "type"
     t.datetime "created_at",               null: false
     t.datetime "updated_at",               null: false
-    t.string   "stc_field_representative"
+    t.string   "stcOfield_representative"
     t.string   "certification_number"
     t.string   "start_date"
     t.string   "end_date"
